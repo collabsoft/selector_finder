de1ed1b5579d33e5e36a2fb4ddf22ce95b22846c
cladea.au@gmail.com <cladea.au@gmail.com>
2021-07-12 09:04:01 +1000

    narrowed manifest

--- chrome_extension.js
+++ chrome_extension.js
@@ -1,2 +1,5 @@
-const chrome_extension = 'chrome-extension://aleacfocnimnddplebbpbfedfagnckcc';
+// const chrome_extension = 'chrome-extension://aleacfocnimnddplebbpbfedfagnckcc'; // live
+
 // const chrome_extension = 'chrome-extension://ojnlcfanjhemfmpolonemdamfloakdhp';
+
+const chrome_extension = 'chrome-extension://plhjgkiapklbinmpdbdgabcgnalhaaio';
