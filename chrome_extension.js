d03b14e62eb18d3c0ad1cc3a0772351bf0b863ef
claudiodea <claudio.angelis@oohmedia.com.au>
2021-07-22 11:36:12 +1000

    removed unused bootstrap

--- chrome_extension.js
+++ chrome_extension.js
@@ -1,5 +1,3 @@
 const chrome_extension = 'chrome-extension://aleacfocnimnddplebbpbfedfagnckcc'; // live
 
-// const chrome_extension = 'chrome-extension://ojnlcfanjhemfmpolonemdamfloakdhp';
-
-// const chrome_extension = 'chrome-extension://plhjgkiapklbinmpdbdgabcgnalhaaio';
+// const chrome_extension = 'chrome-extension://kildjhbmjpchbibopghkgklnmfggbpja';
